22aff06e576ffcdf97ac21d116012899a62d7455
github username <github email address>
2022-02-15 21:10:25 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+
+  # get '/students', to: 'students#index', as: 'students'
+
+  # get '/student/:id', to: 'student#show', as: 'student'
+
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
